e2f2c69f61582299dac0af76d29225bfdb0c3ab0
KishanRaghav <kishanraghav1674@gmail.com>
2026-07-26 20:23:25 +0530

    Time: 2 ms (99.61%), Space: 47.4 MB (82.01%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,22 +1,73 @@
+// class Solution {
+//     public int maximumProduct(int[] nums) {
+//         Arrays.sort(nums);
+//         int max=Integer.MIN_VALUE;
+//         int ans=1;
+//         int ans1=1;
+//         int res=0;
+//         for(int i=0;i<2;i++){
+//             ans*=nums[i];
+//         }
+       
+//         for(int i=nums.length-1;i>nums.length-4;i--){
+//             ans1*=nums[i];
+//         }
+//         if(ans*nums[nums.length-1] > ans1){
+//             res=ans*nums[nums.length-1];
+//         }
+//         else res=ans1;
+        
+//         return res;
+//     }
+// }
+
+
+// class Solution {
+//     public int maximumProduct(int[] nums) {
+//         Arrays.sort(nums);
+
+//         int n = nums.length;
+
+//         return Math.max(
+//             nums[n - 1] * nums[n - 2] * nums[n - 3],
+//             nums[0] * nums[1] * nums[n - 1]
+//         );
+//     }
+// }
+
 class Solution {
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        int max=Integer.MIN_VALUE;
-        int ans=1;
-        int ans1=1;
-        int res=0;
-        for(int i=0;i<2;i++){
-            ans*=nums[i];
+
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+
+            // Update largest three
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+
+            // Update smallest two
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
         }
-       
-        for(int i=nums.length-1;i>nums.length-4;i--){
-            ans1*=nums[i];
-        }
-        if(ans*nums[nums.length-1] > ans1){
-            res=ans*nums[nums.length-1];
-        }
-        else res=ans1;
-        
-        return res;
+
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
     }
 }
